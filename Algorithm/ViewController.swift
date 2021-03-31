87fbc0c4d0c0b53b3590bb44b6624bf371c2c29f
Chiang <liangxq@segimail.com>
2021-03-31 11:28:26 +0800

    希尔排序

--- Algorithm/ViewController.swift
+++ Algorithm/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Chiang on 2021/3/18.
 //
+//心得：1.确定影响的变量个数。2.确定变量的边界值。3.确定指针移动方向
 
 import UIKit
 
@@ -11,11 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var data: [Int] = [5,4,2,6,1,3,7,9,0,8]
+        var data: [Int] = [5,4,2,6,1,3,7,9,0,8,10]
 //        let data: [Int] = [4,3,2,1]
 //        let array = quickSort(array: data, left: 0, right: data.count - 1)
 //        let array = insertSort(array: data)
-        insertSort(array: &data)
+//        insertSort(array: &data)
+        shellSort(array: &data)
         print(data)
     }
 }
@@ -75,7 +77,7 @@ func insertSort(array: [Int]) -> Array<Int> {
     return result
 }
 
-func insertSort(array: inout [Int]){
+func insertSort(array: inout [Int]){//关键字inout
     for i in 1..<array.count {
         let temp = array[i]
         var index: Int? = nil
@@ -88,5 +90,44 @@ func insertSort(array: inout [Int]){
         if index != nil {
             array[index!] = temp
         }
+    }
+}
+
+//shell sort 希尔排序
+//待排数列 87654321 步长为n/2
+// 8 7 6 5      4 3 2 1
+//          >>
+// 4 3 2 1      8 7 6 5
+
+/* 4 3         2 1
+   2 1         4 3
+   8 7    >>   6 5
+   6 5         8 7
+ */
+
+// 2 1 4 3 6 5 8 7  >> 1 2 3 4 5 6 7 8
+
+//[3, 4, 2, 6, 1, 5, 7, 9, 0, 8, 10]
+//[0, 4, 1, 6, 2, 5, 3, 9, 7, 8, 10]
+//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+func shellSort(array: inout [Int]) {
+    var gap = array.count/2
+    while gap > 0 {//gap循环
+        for i in stride(from: gap, to: array.count, by: gap) {//插入排序
+            let temp = array[i]
+            var index: Int? = nil
+            for j in stride(from: i - gap, through: i%gap, by: -gap) {
+                if array[j] > temp {//只要是大于选择基数就都往后移一位
+                    array[j + gap] = array[j]
+                    index = j
+//                    array.swapAt(j + gap, j)
+                }
+            }
+            if index != nil {
+                array[index!] = temp
+            }
+        }
+        gap = gap/2
+        print(array)
     }
 }
